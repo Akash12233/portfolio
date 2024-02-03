7b6cbd46e2ccc4ad49e137c614ceb226c0744657
Akash Arun Tayade <akashtayade1504@gmail.com>
2024-02-03 10:10:06 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+### Description
+
+A simple portfolio template for developer/designers built with React. 
+
+- Fully Responsive
+- Multi-Page Layout
+- Contact Form With EmailJs
+- React-Bootstrap
+- Edit Content From One Place
+
---
+++ src/content_option.js
@@ -0,0 +1,145 @@
+const logotext = "Akash Tayade";
+const meta = {
+    title: "Akash Tayade",
+    description: "I’m Akash Tayade a FullStack Developer and Product Manager",
+};
+
+const introdata = {
+    title: "I’m AKASH TAYADE",
+    animated: {
+        first: "I am a FullStack Developer",
+        second: "I am a Product Manager",
+        third: "I code, I manage, I excel",
+    },
+    description: "As a budding Product Manager and Full Stack Developer embarking on my professional journey, I bring a passion for seamlessly integrating front-end and back-end technologies to create innovative and efficient solutions. With a strong foundation in product management, I strive to deliver dynamic and impactful software experiences.",
+    your_img_url: "./assets/images/photo.png",
+};
+
+const dataabout = {
+    title: "About me",
+    aboutme: "With a versatile skill set spanning Python, C/C++, C#, embedded C, Java, Catkin, Data Structures, Algorithms, and more, I thrive in software development and embrace challenges. Proficient in Agile methodologies, Scrum, and Product Management, I navigate the complexities of backlogs, burndown charts, and comparative analyses. ",
+};
+const worktimeline = [{
+        jobtitle: "Product Intern",
+        where: "AbcoedTech.pvt",
+        date: "2023",
+    },
+    {
+        jobtitle: "Top Coder On Coding-Ninga",
+        where: "VNIT, Nagppur",
+        date: "2023",
+    },
+    {
+        jobtitle: "Qualifier in IIT-G Product Management Competition",
+        where: "Online",
+        date: "2023",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 70,
+    },
+    {
+        name: "Data Structure and Algorithms",
+        value: 90,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "MongoDB",
+        value: 60,
+    },
+    {
+        name:"Node.js",
+        value:60,
+    },
+    {
+        name: "Product Management",
+        value:60,
+    }
+];
+
+const services = [{
+        title: "Web-Development",
+        description: "Experienced in MERN stack, I specialize in crafting efficient and dynamic websites. From MongoDB for flexible data storage to React for interactive UIs, I ensure seamless, full-stack development. Proficient in creating RESTful APIs and implementing responsive designs, my focus on user experience and efficient development practices results in standout websites.",
+    },
+    {
+        title: "Product Management",
+        description: "I specialize in applying MVPs and Agile methodologies, particularly Scrum, for efficient development. Proficient in backlog management and user stories, I navigate complexities to ensure streamlined processes. My commitment to Agile principles guarantees adaptability and collaboration, defining my approach to successful product management.",
+    },
+    {
+        title: "Data Structures and Algorithms",
+        description: " Specializing in advanced structures such as graphs, dynamic programming, and hash maps. With a deep understanding of algorithmic complexities, I consistently excel in solving intricate coding challenges. My expertise extends to crafting efficient solutions, demonstrating a strong command over DSA principles.",
+    },
+];
+
+const dataportfolio = [{
+        heading:"Vehicle Management System",
+        img: require("./assets/images/vrs.png"),
+        description: "A Vehicle Management System is a software application or platform designed to streamline and automate various aspects of managing a fleet of vehicles.",
+        link: "https://github.com/Akash12233/VRS",
+    },
+    {
+        heading:"SportScheduler",
+        img: require("./assets/images/sport.png"),
+        description: "The Sport Scheduler app is a powerful and user-friendly tool designed to streamline the process of organizing and managing sports events, matches, and schedules.",
+        link: "https://github.com/Akash12233/Sport-Sceduler",
+    },
+    {
+        heading:"LinkedIN Tracker",
+        img: require("./assets/images/linkedtracker.png"),
+        description: "Your go-to solution for enhanced LinkedIn networking. Utilizing local storage, it effortlessly stores links for later access.",
+        link: "https://github.com/Akash12233/LinkedIn-Tracker",
+    },
+    {
+        heading:"JPMC Task",
+        img: require("./assets/images/jpmc.png"),
+        description: "The goal of the task is to create a dynamic web application using React.js that reads and processes the data from the CSV file and presents it in the form of interactive graphs and visualizations.",
+        link: "https://github.com/Akash12233/Akash--jpmc-task",
+    },
+    {
+        heading:"Blogse (ongoing)",
+        img: "",
+        description: "This website simplifies the blog writing process, allowing users to effortlessly craft posts on any topic. With an intuitive interface, users can quickly express their thoughts, ideas, and expertise without the complexities of traditional platforms.",
+        link: "https://github.com/Akash12233/blogpost",
+    },
+    {
+        heading:"Reacts (ongoing)",
+        img: "",
+        description: "A dynamic Twitter clone built with React.js. Experience the real-time, interactive nature of Twitter with a sleek and responsive user interface. ReactTweet allows users to tweet, like, and retweet, fostering engaging conversations.",
+        link: "https://github.com/Akash12233/twitter-clone-reactjs",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "akashtayade1504@gmail.com",
+    YOUR_FONE: "+91 8669190245",
+    description: " I seamlessly bridge software and product realms. Fueled by time management, risk-taking, humility, and effective communication, I embody a self-actualized approach to deliver innovative solutions. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+};
+
+const socialprofils = {
+    github: "https://github.com/Akash12233",
+    linkedin: "https://www.linkedin.com/in/akash-tayade-23453322a/",
+    twitter: "https://twitter.com/AkashTa47332668",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
